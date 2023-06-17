6fb5ca5802b213ae58d1323f61f6985b1ed06864
miracle <miraclousyh2000@yahoo.com>
2023-06-16 18:37:49 -0700

    Task update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,14 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function generates a random number and determines
+ *              whether it is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -13,17 +21,17 @@ int main(void)
 	/* your code goes there */
 	if (n == 0)
 {
-	printf("%d is zero", n);
+	printf("%d is zero\n", n);
 	return (0);
 }
 	else if (n < 0)
 {
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 	return (0);
 }
 	else
 {
-	printf("%d is positive", n);
+	printf("%d is positive\n", n);
 }
 
 	return (0);
