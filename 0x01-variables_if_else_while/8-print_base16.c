8be6176a2c6397ad2cf5c9f33ef9a849a04bb895
miracle <miraclousyh2000@yahoo.com>
2023-06-19 17:09:13 -0700

    Task update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 9 ; i++)
+{
+putchar('0' + i);
+}
+for (i = 0 ; i < 6 ; i++)
+{
+putchar('a' + i);
+}
+putchar('\n');
+
+return (0);
+}
