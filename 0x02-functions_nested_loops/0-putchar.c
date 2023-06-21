3fcfe740ad89dcf545073c464c259fb9ffa6520e
miracle <miraclousyh2000@yahoo.com>
2023-06-20 17:23:37 -0700

    Task update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _putchar('_');
