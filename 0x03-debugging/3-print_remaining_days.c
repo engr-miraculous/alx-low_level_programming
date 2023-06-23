8886c7062ec6b739a6af77687b6a3a2309f5dbd1
miracle <miraclousyh2000@yahoo.com>
2023-06-23 02:59:11 -0700

    Task update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,10 +19,6 @@ if (month > 2 || (month == 2 && day == 29))
 printf("Day of the year: %d\n", day + 1);
 printf("Remaining days: %d\n", 366 - (day + 1));
 }
-else
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
 }
 else
 {
