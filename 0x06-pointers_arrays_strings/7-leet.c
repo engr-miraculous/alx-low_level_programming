43b7b814951faed609dd0bee35b7778cd9690b61
miracle <miraclousyh2000@yahoo.com>
2023-06-30 15:38:46 -0700

    Task update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to encode
+ *
+ * Return: A pointer to the encoded string
+ */
+char *leet(char *str)
+{
+int i, j;
+char leet_letters[] = "AEOTL";
+char leet_digits[] = "43071";
+
+i = 0;
+while (str[i] != '\0')
+{
+j = 0;
+while (leet_letters[j] != '\0')
+{
+if (str[i] == leet_letters[j] || str[i] == leet_letters[j] + 32)
+{
+str[i] = leet_digits[j];
+break;
+}
+j++;
+}
+i++;
+}
+
+return (str);
+}
